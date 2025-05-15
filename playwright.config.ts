7599bb9754c5f29ad590d9ea2cea3f86501d3c44
Zahed Bahlak <zahed.bahlak@clever-cloud.com>
2025-05-15 15:55:56 +0200

    Fix: Versions Supported CHnage

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ const pluginE2eAuth = `${dirname(require.resolve('@grafana/plugin-e2e'))}/auth`;
 
 export default defineConfig<PluginOptions>({
   testDir: './tests',
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
